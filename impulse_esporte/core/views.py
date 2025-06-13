3d29120ebb7c6eb6b454eef18444597b023fba3d
guedeees2004-2 <146488719+guedeees2004-2@users.noreply.github.com>
2025-06-13 00:31:25 +0000

    Mudanças do Pedro adicionadas

--- impulse_esporte/core/views.py
+++ impulse_esporte/core/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.views import View
+
+
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
 def index(request):
     context = {
         "title": "Impulse Esporte",
     }
     return render(request, "index.html", context)
+
+class RegisterView(View):
+    def get(self, request):
+        form = CustomUserCreationForm()
+        return render(request, 'registration/register.html', {'form': form})
+
+    def post(self, request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  
+        return render(request, 'registration/register.html', {'form': form})
+
+class LoginView(View):
+    def get(self, request):
+        form = CustomAuthenticationForm()
+        return render(request, 'registration/login.html', {'form': form})
+
+    def post(self, request):
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  
+        return render(request, 'registration/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login') 
