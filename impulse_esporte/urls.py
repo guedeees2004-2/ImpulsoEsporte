a4539c7eeafe215abce1fb1929cca7123a573722
guedeees2004-2 <146488719+guedeees2004-2@users.noreply.github.com>
2025-06-16 02:34:29 +0000

    Adicionando alterações de Guilherme na página do atleta

--- impulse_esporte/urls.py
+++ impulse_esporte/urls.py
@@ -20,27 +20,19 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Vamos importar as views de uma forma mais limpa
-
 from appImpulsoEsporte.views import index, RegisterView, LoginView, user_logout, pagina_atleta
 
 
 urlpatterns = [
-    # 1. Rota da página inicial (index), agora com o nome 'home' para o redirect funcionar
     path("", index, name="home"),
-    
-    # 2. Novas rotas para autenticação
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', user_logout, name='logout'),
-    path("atleta/", pagina_atleta, name="pagina_atleta"),  # <-- Adicione esta linha
-
-    # 3. Rotas que você já tinha
+    path("atleta/", pagina_atleta, name="pagina_atleta"),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
-# Sua configuração de arquivos estáticos e de mídia continua a mesma
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
