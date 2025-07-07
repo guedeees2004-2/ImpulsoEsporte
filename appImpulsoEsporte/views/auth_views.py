d9d00aeb72733073d2106d4e28b543087716af77
GuiVelez06 <guilherme.velez06@gmail.com>
2025-07-07 23:18:40 +0000

    mudei o criei esse auth_views

--- appImpulsoEsporte/views/auth_views.py
+++ appImpulsoEsporte/views/auth_views.py
@@ -1,20 +1,26 @@
+# appImpulsoEsporte/views/auth_views.py
+
 from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.contrib.auth import logout
 from django.shortcuts import redirect
-from appImpulsoEsporte.forms import CustomUserCreationForm
 from django.views.generic import CreateView
 
+from appImpulsoEsporte.forms import CustomUserCreationForm
 
+
+# View de login baseada na classe LoginView do Django
 class LoginView(DjangoLoginView):
     template_name = 'registration/login.html'
 
 
+# View de registro personalizada baseada em CreateView
 class RegisterView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/register.html'
     success_url = '/login/'
 
 
+# View para logout do usuário
 def user_logout(request):
     logout(request)
     return redirect('home')
