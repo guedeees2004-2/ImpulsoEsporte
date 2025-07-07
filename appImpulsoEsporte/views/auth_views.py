f48df329baeea30afaccba821ccd26c896cba024
GuiVelez06 <guilherme.velez06@gmail.com>
2025-07-07 23:12:29 +0000

    criei esse auth_views.py

--- appImpulsoEsporte/views/auth_views.py
+++ appImpulsoEsporte/views/auth_views.py
@@ -1,66 +1,20 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from django.views import View
-
-from ..forms import CustomUserCreationForm, CustomAuthenticationForm
-from ..models import Equipe, EquipeDisponivel, Patrocinador
-
-
-class RegisterView(View):
-    def get(self, request):
-        form = CustomUserCreationForm()
-        return render(request, "registration/register.html", {"form": form})
-
-    def post(self, request):
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            usuario = form.save()
-            # Cria uma equipe se o usuário for do tipo equipe
-            if usuario.tipo_conta == 'equipe':
-                equipe = Equipe.objects.create(
-                    usuario=usuario,
-                    nome=usuario.username,
-                    esporte=form.cleaned_data.get('esporte', ''),
-                    localizacao=form.cleaned_data.get('localizacao', '')
-                )
-                # Cria também no EquipeDisponivel
-                EquipeDisponivel.objects.create(
-                    nome=equipe.nome,
-                    modalidade=equipe.esporte,
-                    cidade=equipe.localizacao,
-                    aberta_para_atletas=True
-                )
-            # Cria um patrocinador se o usuário for do tipo patrocinador
-            elif usuario.tipo_conta == 'patrocinador':
-                Patrocinador.objects.create(
-                    usuario=usuario,
-                    empresa=form.cleaned_data.get('empresa', ''),
-                    cnpj=form.cleaned_data.get('cnpj', ''),
-                    site_empresa=form.cleaned_data.get('site_empresa', ''),
-                    descricao=''
-                )
-            return redirect("login")
-        return render(request, "registration/register.html", {"form": form})
+from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from appImpulsoEsporte.forms import CustomUserCreationForm
+from django.views.generic import CreateView
 
 
-class LoginView(View):
-    def get(self, request):
-        form = CustomAuthenticationForm()
-        return render(request, 'registration/login.html', {'form': form})
+class LoginView(DjangoLoginView):
+    template_name = 'registration/login.html'
 
-    def post(self, request):
-        form = CustomAuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            # Redirecionar baseado no tipo de conta
-            if user.tipo_conta == 'atleta':
-                return redirect('pagina_atleta')
-            else:
-                return redirect('home')
-        return render(request, 'registration/login.html', {'form': form})
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/register.html'
+    success_url = '/login/'
 
 
 def user_logout(request):
     logout(request)
-    return render(request, 'paginaPrincipal.html')
+    return redirect('home')
